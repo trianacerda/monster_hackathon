e6e396017a3be2d42ff3fe4d84a3d2b5c8115a60
joshua360x <jkw5499@psu.edu>
2021-11-16 17:29:38 -0500

    addEvent Listener + looping through nounInputs array + rendering on page

--- app.js
+++ app.js
@@ -8,13 +8,25 @@
 
 const verbInputs = document.getElementsByClassName('verb-input')
 const nounInputs = document.getElementsByClassName('noun-input')
-const adjInputs = document.getElementsByClassName('adj-input')
+const adjectiveInputs = document.getElementsByClassName('adj-input')
 const adverbInputs = document.getElementsByClassName('adverb-input')
+
+
+const verbs = document.getElementsByClassName('verb')
+const nouns = document.getElementsByClassName('noun')
+const adjectives = document.getElementsByClassName('adj')
+const adverbs = document.getElementsByClassName('adverb')
 
 const submitBtn = document.getElementsByClassName('submit')
 
 
 
+submitBtn[0].addEventListener('click', () => {
+  for (i = 0; i < nounInputs.length; i++ ) {
+    nouns[i].textContent = nounInputs[i].value
+    console.log('Nouns', nouns[i]);
+  }
+})
 
 
 
