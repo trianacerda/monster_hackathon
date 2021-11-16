4b253a7bd966d29711ed1ee3581057a2bd8d682b
joshua360x <jkw5499@psu.edu>
2021-11-16 17:10:20 -0500

    grab elements by class names

---
+++ app.js
@@ -0,0 +1,20 @@
+// On Submit we execute events 1 and 2
+// 1. Grab inputs from second paragraph
+
+// 2. Display inputs in new paragraph and hide the first paragraph 
+
+
+// Cache the DOM
+
+const verbInputs = document.getElementsByClassName('verb-input')
+const nounInputs = document.getElementsByClassName('noun-input')
+const adjInputs = document.getElementsByClassName('adj-input')
+const adverbInputs = document.getElementsByClassName('adverb-input')
+
+const submitBtn = document.getElementsByClassName('submit')
+
+
+
+
+
+
